a9eeee9d0e251743c3479575e35dcbada3381bcc
kyonenjs <kyonenjs@icloud.com>
2019-07-11 17:04:09 +0700

    function download asset nested too deeply, refactoring

--- src/download.js
+++ src/download.js
@@ -29,88 +29,75 @@ const create_chapter_folder = (content, course_path) => {
 	}
 };
 
+// Save or download links not from Udemy
+const download_asset_external_link = (asset_name, asset_title, asset) => {
+	const asset_url = asset['external_url'];
+
+	if (asset_title.match(/\b\.\w{1,4}\b/i)) {
+		if (!fs.existsSync(asset_name)) {
+			const stream = got.stream(asset_url, {
+				headers: { 'User-Agent': original_headers['User-Agent'] }
+			});
+			stream.on('response', res => {
+				res.pipe(fs.createWriteStream(asset_name));
+
+				res.on('end', () => {
+					console.log(`\n    ${gray(inverse(' Asset '))}  ${asset_title}  ${green(inverse(' Done '))}`);
+				});
+			}).resume();
+		}
+	} else {
+		try {
+			const data = fs.readFileSync(`${asset_name}.txt`).toString();
+			const urls_in_file = data.split('\n');
+			if (!urls_in_file.includes(asset['external_url'].trim())) {
+				fs.appendFileSync(
+					`${asset_name}.txt`,
+					`${asset['external_url'].trim()}\n`
+				);
+			}
+		} catch (error) {
+			fs.writeFileSync(`${asset_name}.txt`, `${asset['external_url'].trim()}\n`);
+		}
+	}
+};
+
+// Download files with link from Udemy
+const download_asset_file = (asset_name, asset_title, asset) => {
+	const asset_url = asset['download_urls']['File'][0]['file'];
+
+	const stream = got.stream(asset_url, {
+		headers: { 'User-Agent': original_headers['User-Agent'] }
+	});
+	stream.on('response', res => {
+		res.pipe(fs.createWriteStream(asset_name));
+
+		res.on('end', () => {
+			console.log(`\n    ${gray(inverse(' Asset '))}  ${asset_title}  ${green(inverse(' Done '))}`);
+		});
+	}).resume();
+};
+
 const download_supplementary_assets = (content, chapter_path, lecture_index) => {
 	if (content.length > 0) {
 		const asset = content[0];
-		let asset_name = '';
-		let asset_title = '';
+		const asset_title = `${lecture_index} ${asset['title']}`.replace(/[/\\?%*:|"<>]/g, '_');
+		const asset_name = path.join(chapter_path, asset_title);
 
 		if (asset['asset_type'] === 'ExternalLink') {
-			asset_title = `${lecture_index} ${asset['title']}`.replace(/[/\\?%*:|"<>]/g, '_');
-			asset_name = path.join(chapter_path, asset_title);
-			if (asset_name.match(/\b\.\w{1,4}\b/i)) {
-				if (!fs.existsSync(asset_name)) {
-					const stream = got.stream(asset['external_url'], {
-						headers: { 'User-Agent': original_headers['User-Agent'] }
-					});
-					stream.on('response', res => {
-						res.pipe(fs.createWriteStream(asset_name));
-
-						res.on('end', () => {
-							process.stdout.write(
-								`\n    ${gray(
-									inverse(' Asset ')
-								)}  ${asset_title}  ${green().inverse(' Done ')}\n`
-							);
-							content.shift();
-							download_supplementary_assets(
-								content,
-								chapter_path,
-								lecture_index
-							);
-						});
-					}).resume();
-				}
-			} else {
-				try {
-					const data = fs.readFileSync(`${asset_name}.txt`).toString();
-					const urls_in_file = data.split('\n');
-					if (!urls_in_file.includes(asset['external_url'].trim())) {
-						fs.appendFileSync(
-							`${asset_name}.txt`,
-							`${asset['external_url'].trim()}\n`
-						);
-						content.shift();
-						download_supplementary_assets(content, chapter_path, lecture_index);
-					}
-				} catch (error) {
-					fs.writeFileSync(`${asset_name}.txt`, `${asset['external_url'].trim()}\n`);
-					content.shift();
-					download_supplementary_assets(content, chapter_path, lecture_index);
-				}
-			}
+			download_asset_external_link(asset_name, asset_title, asset);
 		}
 
 		if (asset['asset_type'] === 'File') {
-			const asset_url = asset['download_urls']['File'][0]['file'];
-			asset_title = `${lecture_index} ${asset['title']}`.replace(/[/\\?%*:|"<>]/g, '_');
-			asset_name = path.join(chapter_path, asset_title);
-
-			if (!fs.existsSync(asset_name)) {
-				const stream = got.stream(asset_url, {
-					headers: { 'User-Agent': original_headers['User-Agent'] }
-				});
-				stream.on('response', res => {
-					res.pipe(fs.createWriteStream(asset_name));
-
-					res.on('end', () => {
-						process.stdout.write(
-							`\n    ${gray(
-								inverse(' Asset ')
-							)}  ${asset_title}  ${green().inverse(' Done ')}\n`
-						);
-						content.shift();
-						download_supplementary_assets(content, chapter_path, lecture_index);
-					});
-				}).resume();
+			if (fs.existsSync(asset_name)) {
+				console.log(`\n    ${gray(inverse(' Asset '))}  ${asset_title}  ${yellow('(already downloaded)')}`);
 			} else {
-				process.stdout.write(
-					`\n    ${gray(inverse(' Asset '))}  ${asset_title}  ${yellow(
-						'(already downloaded)'
-					)}\n`
-				);
+				download_asset_file(asset_name, asset_title, asset);
 			}
 		}
+
+		content.shift();
+		download_supplementary_assets(content, chapter_path, lecture_index);
 	}
 };
 
