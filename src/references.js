baecffc9ff0c9ea48d525760dfca237ee5b16107
kyonenjs <kyonenjs@icloud.com>
2019-07-28 22:58:25 +0700

    fix 403 error when login with username and password

--- src/references.js
+++ src/references.js
@@ -20,8 +20,7 @@ if (!sub_domain) {
 sub_domain = sub_domain[1];
 
 const headers = {
-	'Origin': 'www.udemy.com',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0',
 	'Accept': '*/*',
 	'Accept-Language': 'en-US,en;q=0.5',
 	'Referer': 'https://www.udemy.com/join/login-popup/',
