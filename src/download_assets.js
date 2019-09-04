c64d28c2453691486c0b043ad0a66159c193b78c
kyonenjs <kyonenjs@icloud.com>
2019-09-04 14:26:52 +0700

    fix user stop downloading cause asset big size corruption

--- src/download_assets.js
+++ src/download_assets.js
@@ -6,41 +6,64 @@ const {headers: original_headers} = require('./references.js');
 const {green_bg, safe_name} = require('./utilities');
 
 // Save or download links not from Udemy
-const download_asset_external_link = (asset_name, asset_title, {external_url: asset_url}) => {
-	if (asset_title.match(/\b\.\w{1,4}\b/i)) {
-		if (!fs.existsSync(asset_name)) {
-			save_asset(asset_url, asset_name, asset_title);
+const download_asset_external_link = (chapter_path, lecture_index, asset) => {
+	const asset_name = safe_name(`${lecture_index} ${asset['title']}`);
+	const asset_name_with_path = path.join(chapter_path, asset_name);
+	const asset_url = asset['external_url'];
+	const asset_id = asset['id'];
+
+	if (asset_name.match(/\b\.\w{1,4}\b/i)) {
+		if (!fs.existsSync(asset_name_with_path)) {
+			save_asset({
+				asset_url,
+				asset_name,
+				asset_id,
+				chapter_path,
+				lecture_index
+			});
 		}
 	} else {
 		try {
-			const data = fs.readFileSync(`${asset_name}.txt`).toString();
+			const data = fs.readFileSync(`${asset_name_with_path}.txt`).toString();
 			const urls_in_file = data.split('\n');
 			if (!urls_in_file.includes(asset_url.trim())) {
-				fs.appendFileSync(`${asset_name}.txt`, `${asset_url.trim()}\n`);
+				fs.appendFileSync(`${asset_name_with_path}.txt`, `${asset_url.trim()}\n`);
 			}
 		} catch (error) {
-			fs.writeFileSync(`${asset_name}.txt`, `${asset_url.trim()}\n`);
+			fs.writeFileSync(`${asset_name_with_path}.txt`, `${asset_url.trim()}\n`);
 		}
 	}
 };
 
 // Download files with link from Udemy
-const download_asset_file = (asset_name, asset_title, asset) => {
+const download_asset_file = (chapter_path, lecture_index, asset) => {
 	const asset_url = asset['url_set']['File'][0]['file'];
+	const asset_name = safe_name(`${lecture_index} ${asset['filename']}`);
+	const asset_id = asset['id'];
 
-	save_asset(asset_url, asset_name, asset_title);
+	save_asset({
+		asset_url,
+		asset_name,
+		asset_id,
+		chapter_path,
+		lecture_index
+	});
 };
 
-const save_asset = (asset_url, asset_name, asset_title) => {
+const save_asset = ({asset_url, asset_name, asset_id, chapter_path, lecture_index}) => {
+	const asset_name_with_path = path.join(chapter_path, asset_name);
+	const downloading_asset_name_with_path = path.join(chapter_path, `${lecture_index} downloading asset ${asset_id}`);
+
 	const stream = got.stream(asset_url, {
 		headers: {'User-Agent': original_headers['User-Agent']}
 	});
 	stream
 		.on('response', res => {
-			res.pipe(fs.createWriteStream(asset_name));
+			res.pipe(fs.createWriteStream(downloading_asset_name_with_path));
 
 			res.on('end', () => {
-				console.log(`\n    ${gray(inverse(' Asset '))}  ${asset_title}  ${green_bg('Done')}`);
+				fs.renameSync(downloading_asset_name_with_path, asset_name_with_path);
+				console.log(`\n    ${gray(inverse(' Asset '))}  ${asset_name}  ${green_bg('Done')}`);
 			});
 		})
 		.resume();
@@ -49,21 +72,19 @@ const save_asset = (asset_url, asset_name, asset_title) => {
 const download_supplementary_assets = (content, chapter_path, lecture_index) => {
 	if (content.length > 0) {
 		const asset = content[0];
-		let asset_title = safe_name(`${lecture_index} ${asset['title']}`);
-		let asset_name = path.join(chapter_path, asset_title);
 
 		if (asset['asset_type'] === 'ExternalLink') {
-			download_asset_external_link(asset_name, asset_title, asset);
+			download_asset_external_link(chapter_path, lecture_index, asset);
 		}
 
 		if (asset['asset_type'] === 'File') {
-			asset_title = safe_name(`${lecture_index} ${asset['filename']}`);
-			asset_name = path.join(chapter_path, asset_title);
+			const asset_name = safe_name(`${lecture_index} ${asset['filename']}`);
+			const asset_name_with_path = path.join(chapter_path, asset_name);
 
-			if (fs.existsSync(asset_name)) {
-				console.log(`\n    ${gray(inverse(' Asset '))}  ${asset_title}  ${yellow('(already downloaded)')}`);
+			if (fs.existsSync(asset_name_with_path)) {
+				console.log(`\n    ${gray(inverse(' Asset '))}  ${asset_name}  ${yellow('(already downloaded)')}`);
 			} else {
-				download_asset_file(asset_name, asset_title, asset);
+				download_asset_file(chapter_path, lecture_index, asset);
 			}
 		}
 
